0c3404857e7e8721f66de5218703c09ceb9eb196
mozinomer <mozinomer12@gmail.com>
2020-10-21 11:12:35 +0500

    started

---
+++ footer.php
@@ -0,0 +1,83 @@
+
+<footer>
+	<div class="footertop">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<h6>Contact</h6>
+					<h2>Take advantage of expert knowledge</h2>
+					<p>Feel free to get in touch with us via email or phone</p>
+					<div class="infoLInks">
+						<a href="mailto:alex.reid@mail.com">alex.reid@mail.com</a>
+						<a href="tel:916-632-3400">916-632-3400</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<a href="#" class="letsTalks">
+						Let’s Talk
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="mainfooter">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="logocontainer">
+						<img src="./images/8.png">
+					</div>
+					<ul class="socialLInksFooter">
+						<li><a href="#">FB</a></li>
+						<li><a href="#">In</a></li>
+						<li><a href="#">tw</a></li>
+						<li><a href="#">ig</a></li>
+						<li><a href="#">yt</a></li>
+					</ul>
+				</div>
+				<div class="col-md-8">
+					<div class="contaienrmenufooter">
+						<div class="menucontainer">
+							<h6>Company</h6>
+							<ul>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Service</a></li>
+								<li><a href="#">Contact us</a></li>
+								<li><a href="#">Resources</a></li>
+							</ul>
+						</div>
+						<div class="menucontainer">
+							<h6>Services</h6>
+							<ul>
+								<!-- <li><a href="#">Automotive</a></li> -->
+								<li><a href="#">Commercial</a></li>
+								<li><a href="#">Residential</a></li>
+								<li><a href="#">Safes</a></li>
+							</ul>
+						</div>
+						<div class="menucontainer">
+							<h6>Policies</h6>
+							<ul>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Terms of use</a></li>
+							</ul>
+							<div class="joinTeam">
+								<h4>Join our team</h4>
+								<a href="#" class="mailjanu">careers@norcallocksmith.com</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyrights">
+		<div class="container">
+			<p>Copyright © 2020 Norcal Locksmith</p>
+		</div>
+	</div>
+</footer>
+
+</body>
+<script src="<?php echo get_template_directory_uri(); ?>/chamanHutiye.js"></script>
+</html>
